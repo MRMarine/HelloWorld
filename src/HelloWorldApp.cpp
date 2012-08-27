f8dfaca7a31c8685926710bdfc9f22a601c3edaf
Matthew Marine <marinemr@muohio.edu>
2012-08-26 23:55:17 -0400

    MouseEvent changes the window color
    MouseEvent changes color for draw().  x/width = red.  y/height = green.
    blue is set using rand().

--- src/HelloWorldApp.cpp
+++ src/HelloWorldApp.cpp
@@ -11,6 +11,8 @@ class HelloWorldApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+  private:
+	float r, g, b;
 };
 
 void HelloWorldApp::setup()
@@ -19,6 +21,16 @@ void HelloWorldApp::setup()
 
 void HelloWorldApp::mouseDown( MouseEvent event )
 {
+	// change the screen color by checking the mouse position in the window
+	// and dividing by the height/width to get a ratio from 0 to 1 for red and green
+	GLint m_viewport[4];
+
+	glGetIntegerv( GL_VIEWPORT, m_viewport );
+	
+	r = (float)(event.getX()) / m_viewport[2];
+	g = (float)(event.getY()) / m_viewport[3];
+	// set the blue ratio to a random float between 0 and 1.
+	b = rand() % 10 / 10.0;
 }
 
 void HelloWorldApp::update()
@@ -27,8 +39,8 @@ void HelloWorldApp::update()
 
 void HelloWorldApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 1.0, 0, 0 ) ); 
+	// clear out the window to the value determined by the RGB set in mouseDown( MouseEvent )
+	gl::clear( Color( r, g, b ) ); 
 }
 
 CINDER_APP_BASIC( HelloWorldApp, RendererGl )
